57c03ad781ab4a9ef19790520ff44011284c323e
myingthungo <55098013+myingthungo@users.noreply.github.com>
2025-03-01 21:38:26 +0530

    Update CardDisplay.jsx

--- src/third/CardDisplay.jsx
+++ src/third/CardDisplay.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import Card from "./Card";
 import { CardImage, CardTitle, CardText } from "./CardComponents";
 import Styles from "./cardDisplay.module.css";
+import UploadIcon from "../images/Uploadicon.png";
 
 export default function CardDisplay() {
   return (
     <div className={Styles.cardBackground}>
       <Card>
-        <CardImage
-          src="./src/images/Uploadicon.png"
-          alt="logo"
-          className={Styles.UploadImage}
-        />
+        <CardImage src={UploadIcon} alt="logo" className={Styles.UploadImage} />
         <CardTitle>
           <h3 className={Styles.title}>Easy Deployment</h3>
         </CardTitle>
